73cc885e42d1dcb5049bbc3b9a418daa6390869e
Julstil <79506162+Julstil@users.noreply.github.com>
2021-02-23 11:41:10 +0100

    HUD, inte kommenterad

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+	public int health;
+
+	public void TakeDamage(int damage)
+	{
+        if (health > 0)
+        {
+			health -= damage;
+			Debug.Log("Health = " + health.ToString());
+        }
+		if (health == 0)
+		{
+			Debug.Log("Player Dead");
+		}
+	}
+
+	public void TakeHealing(int heal)
+	{
+		if (health < 3)
+		{
+			health += heal;
+			Debug.Log("Health = " + health.ToString());
+		}
+		if (health == 3)
+		{
+			Debug.Log("Full Health");
+		}
+	}
+}
+
---
+++ Assets/Scripts/playerHeal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHeal : MonoBehaviour
+{
+	public void SendHealing(int heal)
+	{
+		PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+		playerStats.TakeHealing(heal);
+	}
+}
