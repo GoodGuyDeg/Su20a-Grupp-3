139e42cd5c635979dbfc427a4eaf7089f16418d2
GoodGuyDeg <79506119+GoodGuyDeg@users.noreply.github.com>
2021-02-24 13:50:22 +0100

    Eld, Rep, Bro
    Gjorde en partikel effekt och gav spelaren en eldflamma ovanför huvudet som kan användas för att bränna upp repet så att bron faller. (allt detta gjordes i en ny scen)

---
+++ Assets/CutTheRope.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutTheRope : MonoBehaviour
+{
+    public GameObject bridgerelease; //referens till det som håller uppe bron - Robin 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "playerflame") //Om repet nuddar elden - Robin
+        {
+            transform.position += new Vector3(0, 10000, 0); //Flyttar iväg repet - Robin
+            bridgerelease.transform.position += new Vector3(0, 10000, 0); //Flyttar iväg på collidern som håller uppe bron - Robi
+        }
+    }
+}
